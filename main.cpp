dd1141496d0b47d408784c0b2a43d09512e241f2
BBBrando <bbrandolino@mail.csuchico.edu>
2017-10-18 17:27:10 -0700

    Final Version

---
+++ main.cpp
@@ -0,0 +1,35 @@
+// Brendon Brandolino
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main() {
+vector<vector<char> > starMatrix;
+int rowSize;
+int colSize;
+cout<<"Enter rows and columns of stars"<<endl;
+cin>>rowSize;
+cin>>colSize;
+starMatrix.resize(rowSize);
+
+for(int row = 0; row < starMatrix.size(); row++)
+{
+  starMatrix[row].resize(colSize);
+
+      for(int col = 0; col < starMatrix[row].size() ;col++)
+      {
+        starMatrix[row][col] = '*';
+        cout<<starMatrix[row][col];
+      }
+      cout<<endl;
+}
+
+
+
+
+
+
+
+  return 0;
+}
